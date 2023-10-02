60e7a3f8164c259f3b43838b4707b9a8e9e05472
eduardozacour <89554202+eduardozacour@users.noreply.github.com>
2023-10-02 22:55:26 +0000

    Added email account recovery feature + minor fixes

--- django_project/settings.py
+++ django_project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,12 @@ STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'static'), )
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/users/login'
+
+# Email Confirmation
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'duduvz14@gmail.com'
+EMAIL_HOST_PASSWORD = 'jqoz jkny fbhp xput'
--- django_project/urls.py
+++ django_project/urls.py
@@ -16,8 +16,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from appdoeduardo import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='registration/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='registration/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
     path('users/login/', views.login_user, name="login"),
     path('users/logout/', views.logout_user, name="logout"),
     path('users', views.create_user),
